6d684820ace7fcd9451b4266184c0bb94b7101f6
Zachery Moneypenny <whazzmaster@gmail.com>
2013-06-23 19:08:19 -0500

    Add route for logged-in user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ OmniAuthOnRails::Application.routes.draw do
              path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" },
              controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
+  match "dashboard" => "home#dashboard"
+
   root to: "home#index"
 end
