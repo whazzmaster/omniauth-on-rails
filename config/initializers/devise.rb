52fb1c4abf8ace9f06ca4a1b73422e48ae632c11
Zachery Moneypenny <whazzmaster@gmail.com>
2013-06-23 19:02:06 -0500

    Add oauth initializers to devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -220,6 +220,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
+  config.omniauth :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  config.omniauth :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
